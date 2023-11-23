fc724aec95865e26d412ab8d72d0d17cce877855
Emanuel Bradea <emanuel.bradea@gmail.com>
2023-11-23 18:17:51 +1100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const API_KEY = "812a5afa-4fc9-4685-974f-98c3d7fb93c8";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
